0318aa479bb6047e3c2084037a9a7226aa735dc2
Yuvraj Singh Rathore <ythor203@gmail.com>
2023-11-27 01:03:23 +0530

    django framework message added

--- First/firstapp/views.py
+++ First/firstapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from firstapp.models import Contact
 from datetime import datetime
-
+from django.contrib import messages
 def index(request):
     context={
         'variable':"this is sent"#sending a dictionary with variables and values so they can be accessed by the render
@@ -22,8 +22,9 @@ def contactus(request):
         desc=request.POST.get('desc')
         contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
         contact.save()
+        messages.success(request, "your message has been sent")
 
 
     return render(request,'contactus.html')
-    
+
 
